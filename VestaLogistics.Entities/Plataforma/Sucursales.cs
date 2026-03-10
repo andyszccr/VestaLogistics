870028ca7dc4b252903521f29486dda64fd818f5
andreyszccr <andreyszcr@gmail.com>
2026-03-10 14:37:54 -0600

    clase de Sucursales

---
+++ VestaLogistics.Entities/Plataforma/Sucursales.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VestaLogistics.Entities.Plataforma
+{
+    public class Sucursales
+    {
+        public int Sucursalid { get; set; }
+        public int Empresaid { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
